3586ae8266e46a02ef8b2d01b93bd03d069a44c3
Suelen <suelen_santos2002@outlook.com>
2023-03-16 03:18:17 -0300

    UserResource class findById

--- src/main/java/com/suelen/learningtestingjava/resources/UserResource.java
+++ src/main/java/com/suelen/learningtestingjava/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.suelen.learningtestingjava.resources;
 
 import com.suelen.learningtestingjava.domain.Users;
+import com.suelen.learningtestingjava.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Users> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new Users(1, "suelen", "suelen@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
