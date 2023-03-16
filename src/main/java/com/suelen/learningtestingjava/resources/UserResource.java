e55469ae5233f9877cf490d5ccd2d6afad859299
Suelen <suelen_santos2002@outlook.com>
2023-03-16 04:37:15 -0300

    return dto to UserResource

--- src/main/java/com/suelen/learningtestingjava/resources/UserResource.java
+++ src/main/java/com/suelen/learningtestingjava/resources/UserResource.java
@@ -1,6 +1,6 @@
 package com.suelen.learningtestingjava.resources;
 
-import com.suelen.learningtestingjava.domain.Users;
+import com.suelen.learningtestingjava.domain.dto.UserDTO;
 import com.suelen.learningtestingjava.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Users> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
