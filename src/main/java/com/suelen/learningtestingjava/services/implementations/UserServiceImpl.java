d86c9f9c6cb4b7d93d97051c3e192e14dab6d869
Suelen <suelen_santos2002@outlook.com>
2023-03-16 05:30:26 -0300

    exception treatment update - email already registered

--- src/main/java/com/suelen/learningtestingjava/services/implementations/UserServiceImpl.java
+++ src/main/java/com/suelen/learningtestingjava/services/implementations/UserServiceImpl.java
@@ -38,9 +38,15 @@ public class UserServiceImpl implements UserService  {
         return repository.save(mapper.map(obj, Users.class));
     }
 
+    @Override
+    public Users update(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(mapper.map(obj, Users.class));
+    }
+
     public void findByEmail(UserDTO obj){
         Optional<Users> user = repository.findByEmail(obj.getEmail());
-        if(user.isPresent()){
+        if(user.isPresent() && !user.get().getId().equals(obj.getId())){
             throw new DataIntegratyViolation("Email already registered in the system");
         }
     }
