d0e2b2d3c383b4cffb1bd23594cab756d75fe283
Suelen <suelen_santos2002@outlook.com>
2023-03-16 05:53:38 -0300

    main class test

--- src/test/java/com/suelen/learningtestingjava/LearningTestingJavaApplicationTests.java
+++ src/test/java/com/suelen/learningtestingjava/LearningTestingJavaApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LearningTestingJavaApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        LearningTestingJavaApplication.main(new String[] {});
     }
 
 }
