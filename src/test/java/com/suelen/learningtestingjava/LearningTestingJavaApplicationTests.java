20d13ed7a3961803986a4c5a3a4ebc2bba5a75a3
Suelen <suelen_santos2002@outlook.com>
2023-03-16 00:39:36 -0300

    first commit

---
+++ src/test/java/com/suelen/learningtestingjava/LearningTestingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suelen.learningtestingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTestingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
