cc00d9854ce56ed6833421466c7f417ce9711a0a
Suelen <suelen_santos2002@outlook.com>
2023-03-19 06:44:12 -0300

    UserResource class test - update

--- src/test/java/com/suelen/learningtestingjava/resources/UserResourceTest.java
+++ src/test/java/com/suelen/learningtestingjava/resources/UserResourceTest.java
@@ -100,6 +100,20 @@ class UserResourceTest {
 
     @Test
     void update() {
+        Mockito.when(service.update(userDTO)).thenReturn(user);
+        Mockito.when(mapper.map(Mockito.any(), Mockito.any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
+
+        Assertions.assertEquals(ID, response.getBody().getId());
+        Assertions.assertEquals(NAME, response.getBody().getName());
+        Assertions.assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     @Test
